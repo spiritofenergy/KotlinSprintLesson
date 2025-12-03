f44af7e65f2e61e45a36c8b0d98e8741c09cb4f0
Maric <spiritofenergy220@gmail.com>
2025-12-03 14:54:10 +0300

    KS-0-3 task about space

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -7,7 +7,7 @@ fun main() {
 
     println("Год полета: " + year)
     println("Взлет: " + "0" + hour + " часов")
-    println("Взлет: " + "0" + hour + " минут")
+    println("Взлет: " + "0" + minute + " минут")
 
     hour = 10
     minute = 55
