87e2ad7a9942f5f0a2aa0aa7cfd76824ba775fc6
Maric <spiritofenergy220@gmail.com>
2025-12-06 21:46:43 +0300

    KS-0-7 travelDistance

---
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_1
+
+fun main() {
+    lesson1_task4()
+}
+fun lesson1_task4() {
+    val length: Long = 40868600000 * 1000000
+    val age: Int = 27
+    val partOfDay: Double = 0.075
+    val seconds = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee = 327000 * 1000
+
+    println("Расстояние: " + length)
+    println("Возраст: " + age)
+    println("Часть дня: " + partOfDay)
+    println("Секунды: " + seconds)
+    println("Часть года: " + partOfYear)
+    println("Апогей орбиты: " + apogee)
+
+}
